66e86e3767f891dcab2757d83021b2330dbcf308
RafaelKamada <rafael-kamada2007@hotmail.com>
2025-05-18 17:01:17 -0300

    db ajustes 15

--- outputs.tf
+++ outputs.tf
@@ -26,35 +26,28 @@ output "postgres_connection_string" {
   sensitive   = true
 }
 
-# Outputs para MongoDB
 output "mongodb_endpoint" {
-  description = "Endpoint do MongoDB no EKS"
   value = kubernetes_service.mongodb.status[0].load_balancer[0].ingress[0].hostname
 }
 
 output "mongodb_port" {
-  description = "Porta do MongoDB"
   value = 27017
 }
 
 output "mongodb_username" {
-  description = "Usuário do MongoDB"
   value = "dev_user"
 }
 
 output "mongodb_password" {
-  description = "Senha do MongoDB"
   value = var.mongodb_admin_password
   sensitive = true
 }
 
 output "mongodb_database" {
-  description = "Nome do banco de dados do MongoDB"
   value = "FoodOrder_Cardapio"
 }
 
 output "mongodb_connection_string" {
-  description = "String de conexão completa do MongoDB"
-  value = "mongodb://${aws_db_instance.rds_postgres.username}:${var.mongodb_admin_password}@${kubernetes_service.mongodb.status[0].load_balancer[0].ingress[0].hostname}:27017/FoodOrder_Cardapio"
+  value = "mongodb://${mongodb_username}:${mongodb_password}@${mongodb_endpoint}:${mongodb_port}/${mongodb_database}"
   sensitive = true
 }
--- vars.tf
+++ vars.tf
@@ -35,35 +35,6 @@ variable "rds_arn" {
   default = "arn:aws:rds:us-east-1:276201098979::db:rdsName"
 }
 
-# Variáveis MongoDB
-variable "mongodb_name" {
-  default = "food-order-mongodb"
-}
-
-variable "mongodb_instance_type" {
-  default = "db.t3.small"
-}
-
-variable "mongodb_allocated_storage" {
-  default = 20
-}
-
-variable "mongodb_backup_retention_period" {
-  default = 7
-}
-
-variable "mongodb_subnet_group_name" {
-  description = "Nome do Subnet Group criado na infraestrutura principal"
-  default = "rds_subnet_group"  # Mantendo o mesmo padrão do RDS
-}
-
-variable "mongodb_admin_password" {
-  description = "Password for MongoDB admin user (only for development)"
-  type        = string
-  sensitive   = true
-  default     = "dev123"  # Senha simples para ambiente de desenvolvimento
-}
-
 # Security Group compartilhado para RDS e MongoDB
 variable "db_security_group_id" {
   description = "ID do Security Group criado na infraestrutura principal"
