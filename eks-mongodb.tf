cf1d57059bc6296370f0d07339c7ab72b42f07d4
RafaelKamada <rafael-kamada2007@hotmail.com>
2025-05-18 16:02:08 -0300

    db ajustes 6

--- eks-mongodb.tf
+++ eks-mongodb.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "mongodb" {
   engine                 = "mongodb"
   engine_version         = "6.0.10"
   instance_class         = var.instanceType
-  allocated_storage      = var.mongodb_allocated_storage
+  allocated_storage      = 20
   storage_encrypted      = true
   multi_az              = true
   
@@ -16,7 +16,6 @@ resource "aws_db_instance" "mongodb" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   publicly_accessible    = false
 
-  backup_retention_period = 7
   backup_window          = "03:00-04:00"
   maintenance_window     = "Mon:04:00-Mon:05:00"
   
